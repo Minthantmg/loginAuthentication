bb55894ded279232cff1548ab95cf870c3b99a02
Minthantmg <minthant180@gmail.com>
2024-09-16 14:17:19 +0900

    feat:handle login data

--- src/app/hero/page.tsx
+++ src/app/hero/page.tsx
@@ -1,15 +1,42 @@
-'use client'
-import { useSession } from 'next-auth/react';
-import React from 'react';
-
+"use client";
+import { Button } from "@/components/ui/button";
+import { signOut, useSession } from "next-auth/react";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import React, { useEffect } from "react";
 
 const page = () => {
-    const {data} = useSession();
-    console.log(data);
-    return (
-        <div>
+  const { data } = useSession();
+  const router = useRouter();
+  const userImage = data?.user?.image || "/default-avatar.png";
+  console.log(JSON.stringify(data));
+
+  useEffect(() => {
+    if (!data) {
+      router.push("/Auth/sign-in");
+    }
+  }, [data, router]);
+
+  return (
+    <div>
+      {data ? (
+        <div className="flex flex-col">
+          {data.user?.image ? (
+            <Image src={userImage} alt="user" width={150} height={150} />
+          ) : (
+            <div>No Image Available</div>
+          )}
+          <div className="flex flex-col">
+            <div> Email: {data.user?.email}</div>
+            <div> Username: {data.user?.name}</div>
+            <div>
+              <Button onClick={() => signOut()}>Sign out</Button>
+            </div>
+          </div>
         </div>
-    );
+      ) : null}
+    </div>
+  );
 };
 
 export default page;
